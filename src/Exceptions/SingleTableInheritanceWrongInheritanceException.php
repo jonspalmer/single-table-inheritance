c61e36890651901c4f1e178b7257eb65902c934f
Darron Park <darron1217@gmail.com>
2020-05-21 11:16:45 +0900

    Remove paramters of WrongInheritanceException

--- src/Exceptions/SingleTableInheritanceWrongInheritanceException.php
+++ src/Exceptions/SingleTableInheritanceWrongInheritanceException.php
@@ -4,8 +4,4 @@ namespace Nanigans\SingleTableInheritance\Exceptions;
 
 class SingleTableInheritanceWrongInheritanceException extends SingleTableInheritanceException {
 
-    public function __construct($message, $subClass, $parentClass) {
-        parent::__construct($message . " Subclass: $subClass | Parent: $parentClass");
-    }
-
 } 
